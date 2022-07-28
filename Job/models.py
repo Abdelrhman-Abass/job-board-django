52b6a9f4e5ed7314446b9012fb6e3dc20c1dd6d2
abd01 <abdoabass400@gmail.com>
2022-07-28 17:14:11 +0200

    created Job model

--- Job/models.py
+++ Job/models.py
@@ -1,3 +1,25 @@
+from turtle import title
 from django.db import models
+from django.forms import modelform_factory
 
 # Create your models here.
+
+JOB_TYPE = (
+    ("Full time","Full time"),
+    ("Part time","Part time"),
+)
+
+class job(models.Model):
+    title = models.CharField(max_length=100, blank=True) # column
+    # location
+    job_type = models.CharField(max_length=20 , choices=JOB_TYPE)
+    description = models.TextField(max_length=2000, default=None)
+    published_time = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=1)
+
+
+    def __str__(self):
+        return self.title 
+
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'Blog',
+    'contact',
+    'accounts',
+    'Home',
+    'Job',
+    
 ]
 
 MIDDLEWARE = [
